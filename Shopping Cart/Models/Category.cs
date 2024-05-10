96f8b2d0bb1b335672386a4216b65abfd6b588fe
Yara M Esmaiel <yaraesmaiel123@gmail.com>
2024-05-10 21:45:01 +0300

    Add project files.

---
+++ Shopping Cart/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping_Cart.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30,ErrorMessage ="length can't be above 30 char")]
+        public string Name { get; set; }
+
+    }
+}
